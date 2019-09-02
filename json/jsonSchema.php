123e7c5557da7324c3b9b4b3d2bb0959f6cf0139
Bo Sun <bo.sun@experienceengine.com>
2019-09-02 16:56:19 +0100

    update FullJson

--- json/jsonSchema.php
+++ json/jsonSchema.php
@@ -6,17 +6,16 @@ use \JsonSchema\validator;
 
 $dir = dirname(__FILE__).'/';
 include $dir.'../'."vendor/autoload.php";
-$type = '/booking/request/';
+$type = '/roomAvailablilityCheck/request/';
 
 
 $schema =json_decode(file_get_contents($dir.$type."schema.json"));
-$json = json_decode(file_get_contents($dir.$type."json.json"));
+$json = json_decode(file_get_contents($dir.$type."fullJson.json"));
 
+echo json_last_error_msg() ;
 
-
-
-$a= file_get_contents($dir.$type."json.json");
-$jj = json_decode($a,true);
+// $a= file_get_contents($dir.$type."json.json");
+// $jj = json_decode($a,true);
 // $guestsCount = $jj['guestcounts'];
 // $jj['guestcounts']=new stdClass();
 // $jj['guestcounts']->a=$guestsCount;
@@ -25,7 +24,7 @@ $jj = json_decode($a,true);
 
 
 // echo json_encode($jj);exit;
-echo print_r($jj);exit;
+// echo print_r($jj);exit;
 
 
 $validator = new validator();
