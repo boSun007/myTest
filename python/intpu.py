6b5b4b6f4c693894fc0ee6db6a7a448dd1702074
Bo Sun <bo.sun@experienceengine.com>
2019-10-28 10:23:31 +0000

    daily

---
+++ python/intpu.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# a = input()
+a=100
+if a > 0:
+    print(a)
+else:
+    print(-a)
+
+print('%2d-%03d0' % (3, 1))
+print('%2d-%02d0' % (3, 1))
+print('%.2f' % 3.1415926)
