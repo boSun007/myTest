78d5ac4cfc945e1f0d4453e5bafacdc734cd7969
Bo Sun <bo.sun@experienceengine.com>
2019-10-28 12:25:20 +0000

    daily

--- database/PDO.php
+++ database/PDO.php
@@ -30,9 +30,48 @@ $stmt->execute($id);
 $rtn = $stmt->fetchAll();
 print_r($rtn);
 
+ 
+
+/** insert with last insert id **NOT SCOP_IDENTITY */
+
+$sql = 'INSERT INTO TransactionBlc(Customer,Amount,Currency,Email,Address,City,Tel,Postcode,Country,PSPID,TransTime) VALUES (?,?,?,?,?,?,?,?,?,?,?);';
+$arr = json_decode('["Mr test test","314065","GBP","test@ser.se","asdf","asdfds","01245785477","se123se","UK","epdq7169243","1571754306"]');
+$stmt = $pdo->prepare($sql);
+$result = $stmt->execute($arr);
+echo $pdo->lastInsertId().PHP_EOL;
+
+
+/** insert with last insert id **NOT SCOP_IDENTITY */
+/**instead of INSERTED also available in UPDATED / DELETED */
+$sql = "INSERT INTO TransactionBlc (Customer,Amount,Currency,Email,Address,City,Tel,Postcode,Country,PSPID,TransTime) OUTPUT INSERTED.ID as myLastInsrtedID VALUES (?,?,?,?,?,?,?,?,?,?,?);";
+$arr = json_decode('["Mr test test97","314065","GBP","test@ser.se","asdf","asdfds","01245785477","se123se","UK","epdq7169243","1571754306"]');
+$sth = $pdo->prepare($sql);
+
+$sth->execute($arr);
+
+$result = $sth->fetch(PDO::FETCH_ASSOC);
+
+echo $lastInsertID = $result['myLastInsrtedID'];
+print_r($result);
+
+echo '-------------------------------'.PHP_EOL;
 
 
 
 
+/** simple version */
+$sql = "INSERT INTO TransactionBlc (Customer,Amount,Currency,Email,Address,City,Tel,Postcode,Country,PSPID,TransTime) OUTPUT INSERTED.ID as myLastInsrtedID VALUES ('Mr test test97','314065','GBP','te23st@ser.se','asdf','asdfds','01245785477','se123se','UK','epdq7169243','1571754306');";
+$result = $pdo->query($sql);
+foreach($result as $key => $row){
+    print_r($row);
+}
+
+/** simple version */
+$sql = "INSERT INTO TransactionBlc (Customer,Amount,Currency,Email,Address,City,Tel,Postcode,Country,PSPID,TransTime) OUTPUT INSERTED.ID as myLastInsrtedID VALUES ('Mr test test97','314065','GBP','te23st@ser.se','asdf','asdfds','01245785477','se123se','UK','epdq7169243','1571754306');";
+$result = $pdo->query($sql,PDO::FETCH_NUM);
+
+foreach($result as $row){
+    print_r($row[0]);
+}
 
 
