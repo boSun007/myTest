4c28b69bfebd0c01fc771ff6443263ba60a384c9
Bo Sun <bo.sun@experienceengine.com>
2019-10-31 09:07:52 +0000

    daily

--- array/unset.php
+++ array/unset.php
@@ -10,3 +10,20 @@ if(isset($arr)){
 }else{
     echo 'XX';
 }
+
+$inputErrataData=['Errata'=>'nice'];
+
+$errataData = [
+    'PropertyID' => array_key_exists('PropertyID', $inputErrataData) ? $inputErrataData['PropertyID'] : false,
+    'ErrataStart' => array_key_exists('ErrataStart', $inputErrataData) ? $inputErrataData['ErrataStart'] : false,
+    'ErrataEnd' => array_key_exists('ErrataEnd', $inputErrataData) ? $inputErrataData['ErrataEnd'] : false,
+    'Errata' => array_key_exists('Errata', $inputErrataData) ? $inputErrataData['Errata'] : false,
+    'ErrataPK' => array_key_exists('ErrataPK', $inputErrataData) ? $inputErrataData['ErrataPK'] : false,
+];
+// print_r($errataData);
+
+if (in_array(false, $errataData, true)) {
+ echo 'FFF';
+}else{
+    echo 'xxxx';
+}
