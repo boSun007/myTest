a647429e92645d6734a4279a4a35b6e161715219
bowsl <sunbohouse@gmail.com>
2019-11-07 17:22:57 +0000

    daily

---
+++ functions/session.php
@@ -0,0 +1,13 @@
+<?php
+
+session_start();
+
+$_SESSION['abc']='abc';
+
+// var_dump($_SESSION);
+
+// unset($_SESSION);
+
+// $_SESSION['def']='def';
+
+// var_dump($_SESSION);
---
+++ functions/session2.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+// unset($_SESSION);
+
+// $_SESSION['abc']='abc';
+
+var_dump($_SESSION);
