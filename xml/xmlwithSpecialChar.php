1e3da25f32434ea5e8409d7a23cd41f967c836b3
bowsl <sunbohouse@gmail.com>
2019-11-15 17:30:08 +0000

    daily

--- xml/xmlwithSpecialChar.php
+++ xml/xmlwithSpecialChar.php
@@ -5,8 +5,10 @@ $file = 'xmlwithSpecialChar.xml';
 $string = file_get_contents($dir . '/' . $file);
 $string=preg_replace('/&(?!#?[a-z0-9]+;)/', '&amp;', $string);
 
-
+$string ='<age>20</age>';
 $obj = simplexml_load_string($string);
+var_dump($obj);
 
 
-print_r($obj);
+
+var_dump(json_decode(json_encode($obj),true));
